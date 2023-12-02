cc1a55571acc63f401c6efc8f42708317283729e
AbdurRahman000 <134640054+AbdurRahman000@users.noreply.github.com>
2023-12-02 20:28:07 +0530

    Add files via upload
    Publicaccess specifier main method

---
+++ accessSpecifiers4.java
@@ -0,0 +1,14 @@
+package Pack2forpublicaccessspecifier;
+import Pack1forpublicspecifier.pubaccessspecifiers;
+public class accessSpecifiers4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		pubaccessspecifiers obj = new pubaccessspecifiers(); 
+        obj.display();  
+		
+
+	}
+
+}
