557881d350cfacef46c40c264474fd7e293af1b1
AbdurRahman000 <134640054+AbdurRahman000@users.noreply.github.com>
2023-12-02 19:48:37 +0530

    Add files via upload
    ProtectedAccessSpecifier MaIn function Class

---
+++ accessSpecifiers3.java
@@ -0,0 +1,14 @@
+package Pack2forprotectedspecifierdemo;
+
+import Pack1forprotectedspecifierdemo.proaccessspecifiers;
+
+public class accessSpecifiers3 extends proaccessspecifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		accessSpecifiers3 obj = new accessSpecifiers3 ();   
+	       obj.display();  
+
+	}
+
+}
