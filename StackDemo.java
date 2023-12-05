a467eeb0ce292950dc0c0162582c0e9dc26d3dd8
AbdurRahman000 <134640054+AbdurRahman000@users.noreply.github.com>
2023-12-05 11:03:27 +0530

    Add files via upload

---
+++ StackDemo.java
@@ -0,0 +1,16 @@
+package practicePRojectPhase;
+import java.util.Stack;
+public class StackDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Stack s=new Stack();
+s.push(8);
+s.push(90);
+s.push(60);
+s.push(18);
+System.out.println(s);
+System.out.println("pulled element= "+s.pop());
+	}
+
+}
